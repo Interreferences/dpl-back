e7b5c828e48717e73a80a0e35cc43a2115a4873e
Interreferences <EKZOTuBe@gmail.com>
2024-06-14 09:53:55 +0400

    base setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from "@nestjs/common";
+import {SequelizeModule} from "@nestjs/sequelize";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    controllers: [],
+    providers: [],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'postgress',
+            database: 'music-service',
+            models: [],
+            autoLoadModels: true
+        }),
+    ]
 })
 export class AppModule {}
