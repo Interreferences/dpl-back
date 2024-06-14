022229a9e53c82daf6b53844b73be10d42929825
Interreferences <EKZOTuBe@gmail.com>
2024-06-14 12:31:40 +0400

    Added config for start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import {Module} from "@nestjs/common";
 import {SequelizeModule} from "@nestjs/sequelize";
+import { ArtistsModule } from './artists/artists.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgress',
-            database: 'music-service',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [],
             autoLoadModels: true
         }),
+        ArtistsModule,
     ]
 })
 export class AppModule {}
